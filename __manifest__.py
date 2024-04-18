84e332ed90fe89d4074d563e6914fb033d40d232
growsbyte <167466651+growsbyte@users.noreply.github.com>
2024-04-18 20:23:04 +0200

    Init

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Modules Trackers",
+    'summary': """
+        ModulesTracker is your essential Odoo module for tracking and store development modules """,
+    'description': """
+        ModulesTracker is your essential Odoo module for tracking and store development modules and optimizing workflows. 
+        Monitor activity, analyze performance, and generate insightful reports to streamline your projects effortlessly.
+    """,
+    'author': "GrowsByte",
+    'company': "GrowsByte",
+    'website': "https://www.growsbyte.com/",
+    'license': 'OPL-1',
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base', 'hr'],
+    'installable': True,
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'data/odoo_versions.xml',
+        'views/views.xml',
+        'views/odoo_versions.xml',
+        'views/reporting.xml',
+        'views/configuration.xml',
+        'views/modules_dependents.xml',
+        'views/python_packages_dependents.xml',
+        'views/statistics.xml',
+        'views/menues.xml',
+    ],
+    'css': ['static/src/css/custom.css'],
+}
---
+++ models/reporting.py
@@ -0,0 +1,6 @@
+from odoo import models, fields, api, _
+
+
+class Reporting(models.Model):
+    _name = 'modules.tracker.reporting'
+    _description = 'Modules Tracker Reports'
